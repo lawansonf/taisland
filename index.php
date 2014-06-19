64482571a57a1ab49862afe2fede03d68f4277cc
lawansonf <lawansonf@gmail.com>
2014-06-18 21:06:40 -0500

    Git setup on mac

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Tai's Island</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<h1> Tai's Island</h1>
+	<nav>
+		<ul>
+			<li>Bio</li>
+			<li>Pictues</li>
+			<li>Audio</li>
+			<li>Resume</li>
+			<li>Portfolio</li>
+			<li>Contact</li>
+		</ul>
+	</nav>
+	<main id="wrapper">
+		
+		
+	</main>
+</body>
+</html>
