6d660330aef76b2f808d970a451db2f3040c8254
lawansonf <lawansonf@gmail.com>
2014-06-19 19:34:32 -0500

    Nav Icons updated and logo created.

--- index.php
+++ index.php
@@ -3,14 +3,17 @@
 <head>
 	<title>Welcome to Tai's Island</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/navbar.css">
 </head>
 <body>
-	<h1> Tai's Island</h1>
+	<h1> Ta<img src="images/logo.png" alt="logo" />sland  </h1>
 	<nav>
 		<ul>
 			<li><img src="images/bio.png" alt="bio" /> </li>
 			<li><img src="images/pictures.png" alt="pictures" /> </li>
 			<li><img src="images/music.png" alt="music" /> </li>
+			<li><img src="images/resume.png" alt="music" /> </li>
+			<li><img src="images/contact.png" alt="contact" /> </li>
 	</nav>
 	<main id="wrapper">
 		
